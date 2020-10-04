21536ea59dfe52fed02003c24f5b167b4e154f4c
Tomasz Cichocki <cichymail@gmail.com>
2020-10-04 20:20:35 +0200

    Print Error

--- src/main.rs
+++ src/main.rs
@@ -94,7 +94,13 @@ async fn handle_index(req: Request, _: Context) -> Result<Response<Body>, Error>
         Ok(render_to_json::render_page(&parsed))
     } else if let Some(b) = blob {
         let s = std::str::from_utf8(b.content())?;
-        let cite = Cite::from(s)?;
+        let cite = match Cite::from(s) {
+            Ok(c) => Ok(c),
+            Err(e) => {
+                error!("{}", e);
+                Err(e)
+            }
+        }?;
         debug!("Returning blob response.");
         Ok(render_to_json::render_cite(&cite))
     } else {

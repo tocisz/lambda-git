d79c7bde013e0502f0209fe29f5f7d2a233fa408
Tomasz Cichocki <cichymail@gmail.com>
2020-10-04 12:56:45 +0200

    Access git repository stored in a layer

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 extern crate log;
 
 use lambda_http::{handler, lambda, Body, Context, IntoResponse, Request, RequestExt, Response};
+use std::fs::read_dir;
 
 type Error = Box<dyn std::error::Error + Sync + Send + 'static>;
 
@@ -22,5 +23,13 @@ async fn main() -> Result<(), Error> {
 // }
 
 async fn handle_index(_: Request, _: Context) -> Result<Response<Body>, Error> {
-    Ok("Welcome!".into_response())
+    let mut entries = read_dir("/opt/polacy")?
+        .map(|res| res.map(|e| e.path()))
+        .collect::<Result<Vec<_>, std::io::Error>>()?;
+    let mut ps = vec![];
+    for p in entries {
+        ps.push(String::from(p.to_str().unwrap_or("")))
+    }
+    let j = ps.join(", ");
+    Ok(j.into_response())
 }
